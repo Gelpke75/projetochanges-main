de2498d079752309393d7b3a87b2a5c4a02ac636
gelpke75 <brunogelpk6@gmail.com>
2023-12-20 10:26:24 -0300

    Co-authored-by: Patrick Magalhães <Patrickrj21@users.noreply.github.com>

--- src/app/paginas/loja/loja.page.ts
+++ src/app/paginas/loja/loja.page.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loja',
   templateUrl: './loja.page.html',
   styleUrls: ['./loja.page.scss'],
 })
-export class LojaPage implements OnInit {
+export class LojaPage {
+  
 
-  constructor() { }
-
-  ngOnInit() {
+  buyProduct(product: any) {
+    console.log('Produto comprado:', product);
+    // Adicione aqui a lógica para comprar o produto
   }
-
 }
